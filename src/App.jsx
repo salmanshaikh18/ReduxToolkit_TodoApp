8435daeb38b7d25c5707055a35f036f5acd20373
Salman <salmanshaikh18786@gmail.com>
2023-12-06 13:05:08 +0530

    update Todos, AddTodo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
-import Todo from './components/Todo'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 const App = () => {
   return (
     <>
       <h1 className='text-2xl sm:text-3xl'>Redux Toolkit</h1>
-      <Todo />
+      {/* <AddTodo /> */}
+      <Todos />
     </>
   )
 }
